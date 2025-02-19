3f797509f7f09894bbbf88b72f5fd507b06e78d9
Samra <118079600+SamraAzizi@users.noreply.github.com>
2025-02-19 21:48:33 +0500

    heartDiseaseDetection

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
+from sklearn.naive_bayes import GaussianNB
+
+
 
 df = pd.read_csv('heart.csv')
 
@@ -12,3 +15,6 @@ x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.4, random_
 
 forest = RandomForestClassifier()
 forest.fit(x_train, y_train)
+
+nb_clf = GaussianNB()
+nb_clf.fit(x_train, y_train)
